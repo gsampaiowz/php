214f0b1a598608b98b7fb90c8058aa27efb9a1e9
Gabriel Oliveira Sampaio <gabrielsampaio1216@gmail.com>
2022-02-17 09:03:35 -0300

    Add files via upload

---
+++ aula_01/index.php
@@ -0,0 +1,27 @@
+<?php
+
+//ISSO É UM COMENTÁRIO DE UMA LIHA
+
+/*
+
+ISSO É UM COMENTÁRIO DE VÁRIAS LINHAS
+
+*/
+
+$nome = 'Sampas';
+$idade = 16;
+$trabalhando = false;
+
+echo "<h1>METEU ESSA? $trabalhando";
+echo "<br>";
+echo "<h2>Meu nome é $nome";
+echo "<br>";
+echo "Minha idade é $idade";
+
+$numero = 12;
+
+echo "<br>";
+echo "16 + 12 = ", $idade + $numero;
+echo "<br>";
+var_dump($trabalhando);
+?>
